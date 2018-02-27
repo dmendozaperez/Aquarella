3e8f44a570d0799cf12c76775570f33c8e6baf80
David Mendoza Perez <david_mendozap@hotmail.com>
2018-02-27 14:40:50 -0500

    cambios new febe

---
+++ Integrado/Prestashop/Conexion.cs
@@ -0,0 +1,19 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Integrado.Prestashop
+{
+    public class Conexion
+    {
+        public MySqlConnection getConexionMySQL()
+        {
+            MySqlConnection mysql = new MySqlConnection(ConfigurationManager.ConnectionStrings["mysql"].ConnectionString);
+            return mysql;
+        }
+    }
+}
